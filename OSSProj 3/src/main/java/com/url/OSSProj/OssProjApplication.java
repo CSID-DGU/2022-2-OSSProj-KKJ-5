85e6305b02e229a31c443060ace9341713233ceb
jw0293 <k38836919@gmail.com>
2022-10-14 19:26:22 +0900

    Init Project

---
+++ OSSProj 3/src/main/java/com/url/OSSProj/OssProjApplication.java
@@ -0,0 +1,13 @@
+package com.url.OSSProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssProjApplication.class, args);
+	}
+
+}
