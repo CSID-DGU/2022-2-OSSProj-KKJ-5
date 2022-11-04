a98fd5f3db59289b20b8108f040ba1e0bedb9cbe
jw0293 <k38836919@gmail.com>
2022-11-04 17:08:53 +0900

    refactor : Refactor MemberRepository

--- OSSProj 3/src/main/java/com/url/OSSProj/repository/MemberRepository.java
+++ OSSProj 3/src/main/java/com/url/OSSProj/repository/MemberRepository.java
@@ -10,4 +10,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
     Optional<Member> findByEmail(String email);
 
+    boolean existsByName(String id);
+
 }
