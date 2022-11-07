51cee752ca37f06d05922dbdc52eb652aac2a31b
kim-limit <rlawogks2468@naver.com>
2022-11-07 23:50:14 +0900

    feat: 'signIn service logic'

--- ossproj-react-app/src/services/sign.ts
+++ ossproj-react-app/src/services/sign.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { IResponseAuth, ISignUpProps } from "../interface/user";
+import { IResponseAuth, ISignInProps, ISignUpProps } from "../interface/user";
 
 export const signUp = ({ email, password, name }: ISignUpProps) => {
   const url = `/member`;
@@ -9,4 +9,12 @@ export const signUp = ({ email, password, name }: ISignUpProps) => {
       console.log(res.data);
       return res.data;
     });
+};
+
+export const signIn = ({ email, password }: ISignInProps) => {
+  const url = `/member`;
+  return axios.post<IResponseAuth>(url, { email, password }).then((res) => {
+    console.log(res.data);
+    return res.data;
+  });
 };
