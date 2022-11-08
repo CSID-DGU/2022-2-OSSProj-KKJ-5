ad27523e45b23d9368ce116bf4339df675e28e64
jw0293 <k38836919@gmail.com>
2022-11-08 15:42:17 +0900

    refeat : refact return value MemberDto

--- OSSProj 3/src/main/java/com/url/OSSProj/service/MemberService.java
+++ OSSProj 3/src/main/java/com/url/OSSProj/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.url.OSSProj.service;
 
+import com.url.OSSProj.domain.dto.MemberDto;
 import com.url.OSSProj.domain.dto.SignUpDto;
 import com.url.OSSProj.domain.entity.Member;
+import com.url.OSSProj.domain.enums.UserRole;
 import com.url.OSSProj.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,14 +19,18 @@ public class MemberService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
-    public void signUp(final SignUpDto signUpDto){
-        final Member member = Member.builder()
+    public MemberDto signUp(final SignUpDto signUpDto){
+        memberRepository.save(Member.builder()
                 .name(signUpDto.getName())
                 .email(signUpDto.getEmail())
                 .password(bCryptPasswordEncoder.encode(signUpDto.getPassword()))
-                .build();
+                .role(UserRole.USER)
+                .build());
 
-        memberRepository.save(member);
+        return MemberDto.builder()
+                .name(signUpDto.getName())
+                .email(signUpDto.getEmail())
+                .build();
     }
 
     public Boolean isEmailDuplicated(final String email){
