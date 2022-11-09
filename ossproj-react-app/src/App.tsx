847ff46e6bfdbac2de1dc1650d6719d5b4f63248
kim-limit <rlawogks2468@naver.com>
2022-11-09 17:22:20 +0900

    refact: 'add queryClient'

--- ossproj-react-app/src/App.tsx
+++ ossproj-react-app/src/App.tsx
@@ -3,15 +3,20 @@ import { SignUp } from "./pages/signup";
 import { SignIn } from "./pages/signin";
 import { Home } from "./pages/home";
 import { Chat } from "./pages/chat";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/chat" element={<Chat />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
