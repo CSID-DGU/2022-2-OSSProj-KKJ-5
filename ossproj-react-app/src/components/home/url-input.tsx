8e6b4e90e744a41984398d61488ee80603aa3dfa
kim-limit <rlawogks2468@naver.com>
2022-11-09 17:13:07 +0900

    feat: 'set proxy'

---
+++ ossproj-react-app/src/components/home/url-input.tsx
@@ -0,0 +1,35 @@
+import { IconButton } from "@material-ui/core";
+import { InputBase, Paper } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+import { ChangeEvent } from "react";
+
+interface IUrlInputProps {
+  url: string;
+  handleUrl: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleDelete: () => void;
+}
+
+export const UrlInput = ({ url, handleUrl, handleDelete }: IUrlInputProps) => {
+  return (
+    <Paper
+      component="form"
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder={"url"}
+        inputProps={{ "aria-label": "search google maps" }}
+        value={url}
+        onChange={handleUrl}
+      />
+      <IconButton onClick={handleDelete}>
+        <CloseIcon />
+      </IconButton>
+    </Paper>
+  );
+
+  <IconButton>
+    <SearchIcon />
+  </IconButton>;
+};
--- ossproj-react-app/src/pages/home.tsx
+++ ossproj-react-app/src/pages/home.tsx
@@ -1,36 +1,19 @@
 import { Grid, IconButton, InputBase, Paper, TextField } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from "@mui/icons-material/Close";
+
 import { ChangeEvent, useState } from "react";
+import { UrlInput } from "../components/home/url-input";
 
 export const Home = () => {
   const [url, setUrl] = useState("");
 
   const handleUrl = (e: ChangeEvent<HTMLInputElement>) =>
     setUrl(e.target.value);
-
+  const handleDelete = () => {
+    setUrl("");
+  };
   return (
     <Grid container>
-      <Paper
-        component="form"
-        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
-      >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder={"url"}
-          inputProps={{ "aria-label": "search google maps" }}
-          value={url}
-          onChange={handleUrl}
-        />
-        <IconButton>
-          <CloseIcon />
-        </IconButton>
-      </Paper>
-      {url}
-
-      <IconButton>
-        <SearchIcon />
-      </IconButton>
+      <UrlInput url={url} handleUrl={handleUrl} handleDelete={handleDelete} />
     </Grid>
   );
 };
