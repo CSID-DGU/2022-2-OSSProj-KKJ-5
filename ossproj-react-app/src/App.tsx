0968dcb121614a316e839bdf448fdcade5876b55
kim-limit <rlawogks2468@naver.com>
2022-11-09 14:54:07 +0900

    featL 'craet chat page'

--- ossproj-react-app/src/App.tsx
+++ ossproj-react-app/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { SignUp } from "./pages/signup";
 import { SignIn } from "./pages/signin";
 import { Home } from "./pages/home";
+import { Chat } from "./pages/chat";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/chat" element={<Chat />} />
     </Routes>
   );
 }
---
+++ ossproj-react-app/src/pages/chat.tsx
@@ -0,0 +1,5 @@
+import { Grid } from "@mui/material";
+
+export const Chat = () => {
+  return <Grid></Grid>;
+};
