8f481f94dca71f667b2e2198da2a925c5bc32655
kim-limit <rlawogks2468@naver.com>
2022-11-09 15:57:08 +0900

    feat: 'add websocket with stomp chatting'

--- ossproj-react-app/src/pages/chat.tsx
+++ ossproj-react-app/src/pages/chat.tsx
@@ -1,5 +1,88 @@
 import { Grid } from "@mui/material";
+import * as StompJs from "@stomp/stompjs";
+import { useEffect, useRef, useState } from "react";
+import SockJS from "sockjs-client";
+
+const ROOM_SEQ = 1;
 
 export const Chat = () => {
-  return <Grid></Grid>;
+  const client = useRef<StompJs.Client>();
+  const [chatMessages, setChatMessages] = useState<string[]>([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  }, []);
+
+  const connect = () => {
+    client.current = new StompJs.Client({
+      brokerURL: "ws://localhost:8080/ws-stomp", // 웹소켓 서버로 직접 접속
+      //   webSocketFactory: () => new SockJS("/ws-stomp"), // proxy를 통한 접속
+      connectHeaders: {
+        "auth-token": "spring-chat-auth-token",
+      },
+      debug: function (str) {
+        console.log(str);
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+      onConnect: () => {
+        subscribe();
+      },
+      onStompError: (frame) => {
+        console.error(frame);
+      },
+    });
+
+    client.current.activate();
+  };
+  const disconnect = () => {
+    if (client.current?.deactivate) {
+      client.current.deactivate();
+    }
+  };
+
+  const subscribe = () => {
+    client.current!.subscribe(`/sub/chat/${ROOM_SEQ}`, ({ body }) => {
+      setChatMessages([...chatMessages, JSON.parse(body)]);
+    });
+  };
+
+  const publish = (message: string) => {
+    if (!client.current!.connected) {
+      return;
+    }
+
+    client.current!.publish({
+      destination: "/pub/chat",
+      body: JSON.stringify({ roomSeq: ROOM_SEQ, message }),
+    });
+
+    setMessage("");
+  };
+
+  return (
+    <div>
+      {chatMessages && chatMessages.length > 0 && (
+        <ul>
+          {chatMessages.map((_chatMessage, index) => (
+            <li key={index}>{_chatMessage}</li>
+          ))}
+        </ul>
+      )}
+      <div>
+        <input
+          type={"text"}
+          placeholder={"message"}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => e.which === 13 && publish(message)}
+        />
+        <button onClick={() => publish(message)}>send</button>
+      </div>
+    </div>
+  );
 };
