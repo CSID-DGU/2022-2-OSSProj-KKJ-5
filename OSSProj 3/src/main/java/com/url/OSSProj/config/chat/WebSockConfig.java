75763d30bd80ac97d9a1e497f9687dd5dc83922a
jw0293 <k38836919@gmail.com>
2022-11-10 18:18:39 +0900

    refact : permit wocket connect

--- OSSProj 3/src/main/java/com/url/OSSProj/config/chat/WebSockConfig.java
+++ OSSProj 3/src/main/java/com/url/OSSProj/config/chat/WebSockConfig.java
@@ -18,7 +18,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("http://localhost:3000")
                 .withSockJS();
     }
 }
