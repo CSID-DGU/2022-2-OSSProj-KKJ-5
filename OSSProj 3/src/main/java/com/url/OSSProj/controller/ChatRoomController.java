02238b8184ff7c623791a859e65f793e4ce40888
jw0293 <k38836919@gmail.com>
2022-11-11 03:40:03 +0900

    test : response redirect test

--- OSSProj 3/src/main/java/com/url/OSSProj/controller/ChatRoomController.java
+++ OSSProj 3/src/main/java/com/url/OSSProj/controller/ChatRoomController.java
@@ -5,12 +5,17 @@ import com.url.OSSProj.domain.dto.NewChatRoomDto;
 import com.url.OSSProj.domain.entity.ChatRoom;
 import com.url.OSSProj.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
+@Log4j2
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/chat")
@@ -40,9 +45,11 @@ public class ChatRoomController {
     }
 
     @GetMapping("/room/enter/{roomId}")
-    public String roomDetail(Model model, @PathVariable String roomId){
-        model.addAttribute("roomId", roomId);
-        return "/chat/roomdetail";
+    public String roomDetail(HttpServletRequest request, HttpServletResponse response, @PathVariable String roomId) throws IOException {
+        log.info("roomId : " + roomId);
+        response.sendRedirect("/member/signUp");
+
+        return "hello";
     }
 
     @GetMapping("/room/{roomId}")
