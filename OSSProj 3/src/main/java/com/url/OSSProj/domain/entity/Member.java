c1ac3af8add58a01e7f6f374c4d79caeb64708b2
jw0293 <k38836919@gmail.com>
2022-11-13 15:18:50 +0900

    refact : init DB connect

---
+++ OSSProj 3/src/main/java/com/url/OSSProj/domain/entity/Member.java
@@ -0,0 +1,48 @@
+package com.url.OSSProj.domain.entity;
+
+import com.url.OSSProj.BaseTimeEntity;
+import com.url.OSSProj.domain.enums.UserRole;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseTimeEntity implements Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Setter
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @OneToMany(mappedBy = "url")
+    private List<Url> urls;
+
+    @OneToMany(mappedBy = "member")
+    private List<ChatRoomInfo> memberChatRooms = new ArrayList<ChatRoomInfo>();
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
+
+}
