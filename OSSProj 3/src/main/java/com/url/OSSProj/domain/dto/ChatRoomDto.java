ef32fad94fa9e056019a0dd2c3834249ce0479f2
jw0293 <k38836919@gmail.com>
2022-11-13 14:24:12 +0900

    refact : Add Serializable

--- OSSProj 3/src/main/java/com/url/OSSProj/domain/dto/ChatRoomDto.java
+++ OSSProj 3/src/main/java/com/url/OSSProj/domain/dto/ChatRoomDto.java
@@ -3,9 +3,11 @@ package com.url.OSSProj.domain.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class ChatRoomDto {
+public class ChatRoomDto implements Serializable {
     private String roomId;
     private String name;
 }
