c3003f489cdd04dbad3e6beb7bf78cc6d046a3b8
jw0293 <k38836919@gmail.com>
2022-11-14 23:33:04 +0900

    refact : handler no apply

--- OSSProj 3/src/main/java/com/url/OSSProj/controller/ChatController.java
+++ OSSProj 3/src/main/java/com/url/OSSProj/controller/ChatController.java
@@ -1,16 +1,15 @@
 package com.url.OSSProj.controller;
 
-import com.url.OSSProj.domain.constants.AuthConstants;
 import com.url.OSSProj.domain.dto.ChatMessage;
-import com.url.OSSProj.repository.ChatRoomRepository;
 import com.url.OSSProj.utils.TokenUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RequiredArgsConstructor
 @RestController
 public class ChatController {
@@ -20,13 +19,17 @@ public class ChatController {
     private final RedisTemplate<String, Object> redisTemplate;
 
     @MessageMapping("/chat/message")
-    public void message(ChatMessage message, @Header(AuthConstants.AUTHORIZATION_HEADER) String bearerToken){
-        String name = tokenUtils.getUid(bearerToken.substring(7, bearerToken.length()));
+    public void message(ChatMessage message){
+        String name = message.getSender();
         message.setSender(name);
         if(ChatMessage.MessageType.ENTER.equals(message.getType())) {
             message.setSender("[알림]");
             message.setMessage(name + "님이 입장하셨습니다.");
         }
+
+        log.info("Sender Name : " + name);
+        log.info("Meesage Content : " + message.getMessage());
+        log.info("RoomId : " + message.getRoomId());
         redisTemplate.convertAndSend(channelTopic.getTopic(), message);
     }
 }
