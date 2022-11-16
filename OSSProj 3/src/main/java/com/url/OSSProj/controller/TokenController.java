f59b3c833f5340c08b25a3f3fb364bb696fe1fd9
jw0293 <k38836919@gmail.com>
2022-11-17 00:56:46 +0900

    feat : Regenerate AccessToken from RefreshToken

--- OSSProj 3/src/main/java/com/url/OSSProj/controller/TokenController.java
+++ OSSProj 3/src/main/java/com/url/OSSProj/controller/TokenController.java
@@ -2,14 +2,19 @@ package com.url.OSSProj.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.url.OSSProj.domain.constants.AuthConstants;
+import com.url.OSSProj.domain.dto.SuccessLoginMemberDto;
 import com.url.OSSProj.domain.dto.Token;
+import com.url.OSSProj.domain.entity.Member;
 import com.url.OSSProj.domain.enums.UserRole;
+import com.url.OSSProj.repository.MemberRepository;
+import com.url.OSSProj.utils.CookieUtils;
 import com.url.OSSProj.utils.TokenUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -19,7 +24,9 @@ import java.io.IOException;
 public class TokenController {
 
     private final TokenUtils tokenUtils;
+    private final CookieUtils cookieUtils;
     private final ObjectMapper objectMapper;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/token/expired")
     public String auth(){
@@ -28,23 +35,35 @@ public class TokenController {
 
     @PostMapping("/token/refresh")
     public String refreshAuth(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Cookie refreshCookie = cookieUtils.getCookie(request, AuthConstants.REFRESH_HEADER);
+        String refreshToken = refreshCookie.getValue();
 
-        String refreshToken = request.getHeader(AuthConstants.REFRESH_HEADER);
-
-        if(refreshToken != null && tokenUtils.isValidToken(refreshToken)){
+        if(refreshToken != null && tokenUtils.isValidToken(refreshToken)) {
             String email = tokenUtils.getUid(refreshToken);
             Token newToken = tokenUtils.generateToken(email, UserRole.USER.getKey());
+            Cookie refreshTokenCookie = cookieUtils.createCookie(AuthConstants.REFRESH_HEADER, newToken.getRefreshToken());
 
-            response.addHeader(AuthConstants.AUTH_HEADER, newToken.getAccessToken());
-            response.addHeader(AuthConstants.REFRESH_HEADER, newToken.getRefreshToken());
-            response.setContentType("application/json;charset=UTF-8");
+            response.addCookie(refreshTokenCookie);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
 
-            var writer = response.getWriter();
+            Member member = memberRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("no such data"));
 
-            String allToken = objectMapper.writeValueAsString(newToken);
+            final SuccessLoginMemberDto successLoginMemberDto = getSuccessLoginMemberDto(newToken, member);
 
-            return "OK NEW TOKEN";
+            String loginMemberJsonResponse = objectMapper.writeValueAsString(successLoginMemberDto);
+            response.getWriter().write(loginMemberJsonResponse);
+
+            return "Success New Token";
         }
         throw new RuntimeException();
+    }
+
+    private SuccessLoginMemberDto getSuccessLoginMemberDto(Token newToken, Member member) {
+        final SuccessLoginMemberDto successLoginMemberDto = new SuccessLoginMemberDto();
+        successLoginMemberDto.setName(member.getName());
+        successLoginMemberDto.setAccessToken(newToken.getAccessToken());
+
+        return successLoginMemberDto;
     }
 }
