0d361312d4467ddf80e5f5048ba15801fef24a49
jw0293 <k38836919@gmail.com>
2022-11-17 13:17:18 +0900

    build : to pull

--- ossproj-react-app/src/App.tsx
+++ ossproj-react-app/src/App.tsx
@@ -17,14 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-<<<<<<< HEAD
-
           <Route path="/chat" element={<Chat />} />
-          <Route path="/chat/room/enter/1" element={<Chat />} />
-
-=======
-          <Route path="/chat" element={<Chat />} />
->>>>>>> d676399943e7b0447193b4d60d09a1bb59c00107
         </Routes>
       </QueryClientProvider>
     </UserProvider>
--- ossproj-react-app/src/pages/chat.tsx
+++ ossproj-react-app/src/pages/chat.tsx
@@ -113,10 +113,7 @@ export const Chat = () => {
     });
     client.current!.connect(
       {
-<<<<<<< HEAD
 
-=======
->>>>>>> d676399943e7b0447193b4d60d09a1bb59c00107
         Authorization: axios.defaults.headers.common["Authorization"],
       },
       () => {
@@ -124,10 +121,6 @@ export const Chat = () => {
           setChatMessage(message.body);
         });
       }
-<<<<<<< HEAD
-
-=======
->>>>>>> d676399943e7b0447193b4d60d09a1bb59c00107
     );
 
     setChatName(id);
@@ -138,31 +131,23 @@ export const Chat = () => {
     client.current!.send(
       "/pub/chat/message",
       {
-<<<<<<< HEAD
-        token:
-          // "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMDgwMSIsImlhdCI6MTU1MTY2NzA0NCwiZXhwIjoxNTUxNjY4ODQ0fQ.Ncqvem4RlCwITDgFvT3GPvTcQNsSeysR1SYkGi4PVSpqkxFHDQt4liJGfO0SYMLTOD90zHC0vX47wT0WROE6dQ",
-        axios.defaults.headers.common["Authorization"]
-=======
+
         Authorization: axios.defaults.headers.common["Authorization"],
->>>>>>> d676399943e7b0447193b4d60d09a1bb59c00107
+
       },
       JSON.stringify({
         type: "TALK",
         roomId: "1",
         sender: "김재한",
         message: message,
-<<<<<<< HEAD
 
-=======
->>>>>>> d676399943e7b0447193b4d60d09a1bb59c00107
+
+
+
       })
     );
     setMessage("");
   };
-<<<<<<< HEAD
-
-=======
->>>>>>> d676399943e7b0447193b4d60d09a1bb59c00107
   return (
     <Grid
       container
