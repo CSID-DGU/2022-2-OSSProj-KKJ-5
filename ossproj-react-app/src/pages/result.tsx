0ddaece4ea5df97dad5264ccf4fc68d13de32756
kim-limit <rlawogks2468@naver.com>
2022-11-27 13:07:46 +0900

    refact:' set mock image'

--- ossproj-react-app/src/pages/result.tsx
+++ ossproj-react-app/src/pages/result.tsx
@@ -1,9 +1,10 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { MenuBar } from "../components/commons/menu-bar";
 import { useSummaryContent } from "../hooks/use-summary-url";
-
+import wordcloud from "../assets/wordcloud.png";
+import network from "../assets/network.png";
 export const Result = () => {
   const { search } = useLocation();
   const query = search.split(/[=,&]/)[1];
@@ -21,7 +22,7 @@ export const Result = () => {
       paddingLeft={`100px`}
       paddingRight={`50px`}
       paddingBottom={`100px`}
-      bgcolor={"#e5e5e5"}
+      // bgcolor={"#e5e5e5"}
       direction={{ lg: "row", md: "row", sm: "column", xs: "column" }}
     >
       {/* menu grid */}
@@ -37,15 +38,32 @@ export const Result = () => {
         direction={"column"}
         border={"1px solid black"}
       >
-        <Grid item lg={1} md={1} sm={1} xs={1}>
-          {data?.url}
+        <Grid
+          item
+          lg={1}
+          md={1}
+          sm={1}
+          xs={1}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Box
+            width={"90%"}
+            height={"50%"}
+            //  border={"1px solid black"}
+          >
+            <a href={data?.url}>
+              <Typography>{data?.url}</Typography>
+            </a>
+          </Box>
         </Grid>
         {/* image grid */}
-        <Grid item lg={8} md={8} sm={8} xs={8} border={"1px solid black"}>
+        <Grid item lg={8} md={8} sm={7} xs={7} border={"1px solid black"}>
           <Box
             sx={{ overflowY: "scroll" }}
             width={"100%"}
-            height={"55vh"}
+            height={"50vh"}
             display={"flex"}
             flexDirection={{ lg: "row", md: "row", sm: "column", xs: "column" }}
             alignItems={"center"}
@@ -54,15 +72,17 @@ export const Result = () => {
             <Box
               width={{ lg: "40%", md: "40%", sm: "90%", xs: "90%" }}
               height={"90%"}
-              border={"1px solid black"}
+              // border={"1px solid black"}
             >
-              {data?.image}
+              <img src={wordcloud} width={"100%"} height={"100%"} />
             </Box>
             <Box
               width={{ lg: "40%", md: "40%", sm: "90%", xs: "90%" }}
               height={"90%"}
-              border={"1px solid black"}
-            ></Box>
+              // border={"1px solid black"}
+            >
+              <img src={network} width={"100%"} height={"100%"} />
+            </Box>
           </Box>
         </Grid>
         {/* text grid */}
@@ -70,14 +90,19 @@ export const Result = () => {
           item
           lg={3}
           md={3}
-          sm={4}
-          xs={4}
+          sm={3}
+          xs={3}
           display={"flex"}
           justifyContent={"center"}
           alignItems={"center"}
         >
-          <Box width={"90%"} height={"80%"} border={"1px solid black"}>
+          <Box
+            width={"90%"}
+            height={"80%"}
+            //  border={"1px solid black"}
+          >
             {data?.content}
+            {/* <Typography>{"sadfsafasdfasdfasdfasdfasdfsfsfsd"}</Typography> */}
           </Box>
         </Grid>
       </Grid>
